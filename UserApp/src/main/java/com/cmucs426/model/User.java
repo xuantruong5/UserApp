005e303d2b7c97aa6f64cb2eed3c2a75b4ca5e9b
xuantruong5 <xuantruong0812@gmail.com>
2025-03-11 20:52:49 +0700

    Initial commit

---
+++ UserApp/src/main/java/com/cmucs426/model/User.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cmucs426.model;
+
+/**
+ *
+ * @author truong
+ */
+public class User {
+    
+    private String userId;
+    private String email;
+
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+
+    public void updateEmail(String newEmail) { 
+        this.email = email; 
+    }
+}
+
+
